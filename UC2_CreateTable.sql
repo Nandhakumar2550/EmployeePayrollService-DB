5233c0cbb352c6d0716943aa947a5caaa9d01288
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-05-28 18:35:33 +0530

    [Nandhakumar]/UC2_CreateTable

---
+++ UC2_CreateTable.sql
@@ -0,0 +1,10 @@
+USE payroll_service;
+CREATE TABLE employee_payroll (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    salary DOUBLE NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+SHOW TABLES;
+DESC employee_payroll;
